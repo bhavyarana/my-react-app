fe615a8d3257d74933baa8e06aec8afed4e43009
Bhavya Rana <bhavya.rana10@gmail.com>
2023-04-28 19:37:43 +0530

    my react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bhavya Rana
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to my react app
         </a>
       </header>
     </div>
